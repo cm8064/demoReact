4df9fb0c9ae74df37e0a0d8e7799abf87f607523
cm8064 <34864884+cm8064@users.noreply.github.com>
2025-05-17 15:55:05 -0500

    Commit Inicial

---
+++ react2/ucp-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Test de prueba</h1>
+       <h2>A la una </h2>
+       <h3>A las dos</h3>
+       <h3>A las tres</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
